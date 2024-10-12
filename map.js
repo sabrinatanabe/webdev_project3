eb420b09f55defeca63225a326ba43e2f0e1a6c6
sabrinatanabe <sabrinatanabe@gmail.com>
2024-10-12 00:00:05 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,8 +1,8 @@
 function initMap(){
 	//alert('it works');
-	var el=document.getElementById('canvas');
-	var myLocation=new google.maps.LatLng(41.835117, -87.627130);
-	var mapOptions= {
+	var el = document.getElementById('canvas');
+	var myLocation = new google.maps.LatLng(41.835117, -87.627130);
+	var mapOptions = {
 		center: myLocation,
 		zoom: 18,
 		mapTypeId: google.maps.MapTypeId.SATELLITE,
@@ -11,9 +11,9 @@ function initMap(){
 		}
 	};
 	
-	var myMap=new google.maps.Map(el, mapOptions);
+	var myMap = new google.maps.Map(el, mapOptions);
 	
-	var marker=new google.maps.Marker({
+	var marker = new google.maps.Marker({
 		position: myLocation,
 		map: myMap
 		animation: google.maps.Animation.DROP,
@@ -22,7 +22,7 @@ function initMap(){
 	
 	var contentString='<h1>Location</h1>';
 	
-	var infowindow=new google.maps.InfoWindow({
+	var infowindow = new google.maps.InfoWindow({
 			content: contentString
 		}) 
 		
