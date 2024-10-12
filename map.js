613b1a45f4382aa4bb4545d902a05d85def34fd0
sabrinatanabe <sabrinatanabe@gmail.com>
2024-10-11 22:57:24 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,8 +1,8 @@
 function initMap(){
 	//alert('it works');
 	var el=document.getElementById('canvas');
-	var myLocation = new google.maps.LatLng(41.835117, -87.627130);
-	var mapOptions = {
+	var myLocation=new google.maps.LatLng(41.835117, -87.627130);
+	var mapOptions= {
 		center: myLocation,
 		zoom: 18,
 		mapTypeId: google.maps.MapTypeId.SATELLITE,
@@ -11,19 +11,26 @@ function initMap(){
 		}
 	};
 	
-	var myMap = new google.maps.Map(el, mapOptions);
+	var myMap=new google.maps.Map(el, mapOptions);
 	
-	var marker = new google.maps.Marker({
+	var marker=new google.maps.Marker({
 		position: myLocation,
 		map: myMap
 		animation: google.maps.Animation.DROP,
 		icon: 'italy.jpg'
 	});
 	
-	var map=new google.maps.Map(document.getElementById('map),	{
-			center: {lat: -34.397, lng: 150.644},
-			zoom: 8
-		});
-}	
+	var contentString='<h1>Location</h1>';
+	
+	var infowindow=new google.maps.InfoWindow({
+			content: contentString
+		}) 
+		
+	google.maps.event.addListener(marker,'mouseover', function() {
+		infowindow.open(myMap, marker);
+	});	
+	
+	
+}
 
 google.maps.event.addDomListener(window, 'load', init);
